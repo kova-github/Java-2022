92b8b3ffe5a62153676b6227c46add325d6ef9d2
Nastya Kovaleva <59012012+kova-github@users.noreply.github.com>
2022-04-16 04:08:26 +0300

    java practice 2022

---
+++ src/main/Start2.java
@@ -0,0 +1,16 @@
+public class Start2 {
+    public static void main (String args[] ){
+        // 8  прмитивных типов данных
+        byte num = 100; // 8 bit  -128 .. +127
+        short num1 = 131; //16 bit -32688..+32687
+        int num2 = 141; //32 bit -2147483648..+214748347
+        long num3 = 151; //64 bit –9 223 372 036 854 775 808 .. +9 223 372 036 854 775 807
+
+        float num4 = 1.1f; // 32 bit (6-7 знаков после запятой)
+        double num5 = 1.11; // 64 bit (15 знаков после запятой)
+
+        char sym = 'a'; //16 bit
+        boolean isTrue = false; // 1 bit true
+
+    }
+}
---
+++ src/main/Start3.java
@@ -0,0 +1,17 @@
+public class Start3 {
+    private static int num1 = 121;
+    public static int num2 = 131;
+    public static final int num3 = 151;
+    public static void main (String args[]){
+        // переменные
+        int num = 100;
+        int a;
+        a = 101;
+        int b, c, d;
+        num2 = 142;
+        int res = num2 + num3;
+        int res1 = num1 + a + num;
+        System.out.println(res + res1);
+        System.out.println(res + "  " + res1);
+    }
+}
